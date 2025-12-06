331eabd1b45aa7a691c1d4c59e0dae8814f77f39
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-06 14:55:43 +0500

    Create dubbing.js

---
+++ src/utils/dubbing.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const dubVideo = async (videoUrl, language='fa') => {
+  // Replace with real dubbing API integration
+  return `${videoUrl}?dubbed=${language}`;
+};
