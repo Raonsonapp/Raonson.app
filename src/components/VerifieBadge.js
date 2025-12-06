0288fe26f9ba544c294fa441dd5a4e72528ec86a
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-06 15:01:26 +0500

    Delete src/components directory

--- src/components/VerifieBadge.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Image, StyleSheet } from 'react-native';
-
-export default function VerifiedBadge() {
-  return <Image source={require('../assets/images/verified_badge.png')} style={styles.badge} />;
-}
-
-const styles = StyleSheet.create({ badge: { width: 20, height: 20 } });
