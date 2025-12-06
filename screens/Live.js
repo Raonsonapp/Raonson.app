ee8b701fd6dddb8fa341ad1ea21dc5dd4c727364
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-06 14:52:52 +0500

    Create Live.js

---
+++ screens/Live.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Live() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Live Streaming will appear here</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({ container: { flex: 1, justifyContent: 'center', alignItems: 'center' }, text: { fontSize: 18 } });
